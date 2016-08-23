fc7b7205c03fffc0a51eaa0e764484c8594b85b6
Roy <b01501061@gmail.com>
2016-08-23 18:14:12 +0800

    fix plot error

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from keras import backend as K
 from sklearn.manifold import TSNE
 from keras.utils import np_utils
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 from vis import scatter
 
 def get_output(i, model, name, X, Y):
@@ -11,7 +12,5 @@ def get_output(i, model, name, X, Y):
 
     # We pick first 1000 points to do TSNE
     reduced_layer_output = tsne_model.fit_transform(get_layer_output([X])[0][:500])
-    #plt.figure(figsize=(12,12))
-    #plt.title(name, fontsize=20)
-    plt = scatter(reduced_layer_output, np_utils.categorical_probas_to_classes(Y[:500]))
-    plt.savefig('./img/' + name)
+    plt2 = scatter(reduced_layer_output, np_utils.categorical_probas_to_classes(Y[:500]))
+    plt2.savefig('./img/' + name)
