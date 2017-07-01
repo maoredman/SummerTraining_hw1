26c6d5eb0234fd37cb2d5bea9df197745f4242f5
maoredman <ccm29cam@gmail.com>
2017-07-01 11:40:23 +0800

    categorical_probas_to_classes no longer supported by Keras

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 from keras import backend as K
 from sklearn.manifold import TSNE
-from keras.utils import np_utils
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 from vis import scatter
+import numpy as np
 
 def get_output(i, model, name, X, Y):
     # Build TSNE model
@@ -12,5 +12,5 @@ def get_output(i, model, name, X, Y):
 
     # We pick first 1000 points to do TSNE
     reduced_layer_output = tsne_model.fit_transform(get_layer_output([X])[0][:500])
-    plt2 = scatter(reduced_layer_output, np_utils.categorical_probas_to_classes(Y[:500]))
+    plt2 = scatter(reduced_layer_output, np.argmax(Y[:500], axis=1))
     plt2.savefig('./img/' + name)
